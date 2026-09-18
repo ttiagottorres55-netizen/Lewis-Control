466f8563489acbd7196fa64c2ce6c981da861bf9
ttiagottorres55-netizen <ttiagottorres55@gmail.com>
2026-09-17 21:03:43 -0300

    Change base URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {VitePWA} from 'vite-plugin-pwa';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/Lewis-Control/',
     plugins: [
     plugins: [
       react(),
